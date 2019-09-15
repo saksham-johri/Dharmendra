3c655e0f636fc8a747f4a29c3b0bc88518ce036a
Saksham Johri <sakshamjohri@gmail.com>
2019-09-16 00:38:58 +0530

    16th Sept 2019

--- mail.php
+++ mail.php
@@ -3,7 +3,7 @@
     $name = $_POST["name"];
     $email= $_POST["email"];
     $text= $_POST["message"];
-    $subject= $_POST["subject"];
+
     
 
 
@@ -12,16 +12,20 @@
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
     $message ='<table style="width:100%">
-        <tr>
-            <td>'.$name.'  '.$subject.'</td>
-        </tr>
-        <tr><td>Email: '.$email.'</td></tr>
-        <tr><td>phone: '.$subject.'</td></tr>
-        <tr><td>Text: '.$text.'</td></tr>
+                <tr>
+                    <td>Name:  '.$subject.'</td>
+                </tr>
+                <tr>
+                    <td>Email: '.$email.'</td>
+                </tr>
         
-    </table>';
+                <tr>
+                    <td>Text: '.$text.'</td>
+                </tr>
+        
+                </table>';
 
-    if (@mail($to, $email, $message, $headers))
+    if (@mail($to, $message, $headers))
     {
         echo 'Your message has been sent.';
     }else{
